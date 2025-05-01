aab7a34f8d8be8eff29f8d76d029e6900dff0ca2
yiktllw <151817116+yiktllw@users.noreply.github.com>
2025-05-01 22:22:02 +0800

    feat: add blueaka font from [kivo wiki](https://kivo.fun)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import "./assets/dracula.css";
 import "./assets/stackoverflow-light.css";
 import "./assets/blog.scss";
 import "./assets/font-awesome/fa.css";
+import "./assets/Blueaka/Blueaka.css";
+import "./assets/Blueaka_Bold/Blueaka_Bold.css";
 import "nprogress/nprogress.css";
 import App from "./App.vue";
 import { router } from "./router";
